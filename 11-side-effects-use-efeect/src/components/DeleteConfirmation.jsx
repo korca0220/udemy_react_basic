4f8c6abfa5071629ef70a4d860669fa1330df438
ParkJuneWoo <krca0220@gmail.com>
2024-06-12 22:23:35 +0900

    progress interval

--- 11-side-effects-use-efeect/src/components/DeleteConfirmation.jsx
+++ 11-side-effects-use-efeect/src/components/DeleteConfirmation.jsx
@@ -1,11 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  useEffect(() => {
+    console.log("INTERVAL");
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      console.log("INTERVAL CLEARED");
+      clearInterval(interval);
+    };
+  }, []);
+
   useEffect(() => {
     console.log("TIMER SET");
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     // use effect clean up function
     // DOM에서 삭제되기 전에 수행
@@ -27,6 +43,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
